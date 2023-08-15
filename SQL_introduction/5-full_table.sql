cf1be55240774d0cce62023c2a9d5146be6ce4fd
Aderynlola <125492983+Aderynlola@users.noreply.github.com>
2023-08-15 22:29:02 +0100

    Task 6

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,4 +1,7 @@
 -- a script that prints the full description of the table first_table 
 --from the database hbtn_0c_0 in your MySQL server.
 
-SHOW CREATE TABLE first_table;
+SELECT table_name, create_statement
+FROM information_schema.tables
+WHERE table_schema = 'hbtn_0c_0'
+  AND table_name = 'first_table';
