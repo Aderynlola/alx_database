4d76c2aec0a48c2e7df9522d06fdabb6d545bcab
Aderynlola <125492983+Aderynlola@users.noreply.github.com>
2023-08-16 20:11:07 +0100

    Task 9

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,2 +1,4 @@
---a script that lists all the tables of a database in my MySQL server
+-- a script that lists all the tables of a database in my MySQL server
+
+-- shows a list of all tables in the database
 SHOW TABLES;
