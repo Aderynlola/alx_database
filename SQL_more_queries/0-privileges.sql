646779bad02973b011f51187de3071cc7a9ffbf2
Aderynlola <125492983+Aderynlola@users.noreply.github.com>
2023-08-17 08:50:24 +0100

    Task 0

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,5 @@
+-- a script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server
+-- it lists all granted privileged for one user in a database
+SHOW GRANTS FOR user_0d_1@localhost;
+-- it lists all granted privileged for one user in a database
+SHOW GRANTS FOR user_0d_2@localhost;
